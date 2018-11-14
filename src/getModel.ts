76f17c126e67b95789e2d078fe5f277cefc48a21
Olivier Bourdoux <olivier.bourdoux@gmail.com>
2018-11-14 14:02:01 +0100

    schemaOptions - allow non @rest models to use getModel()

--- src/getModel.ts
+++ src/getModel.ts
@@ -10,17 +10,18 @@ import { RestRegistry } from './RestRegistry';
  */
 export function getModel<T extends Typegoose>(connection: Connection, model: Constructor<T>): Model<InstanceType<T>> {
     const modelEntry = RestRegistry.getModel(model);
+    const schemaOptions = modelEntry && modelEntry.config ? modelEntry.config.schemaOptions : undefined;
 
     if (!connection.models[model.name]) {
         //const schema = model.prototype.buildSchema(model.name);
         // get schema of current model
-        let schema = model.prototype.buildSchema(model.name, modelEntry.config.schemaOptions);
+        let schema = model.prototype.buildSchema(model.name, schemaOptions);
         // get parents class name
         let parentCtor = Object.getPrototypeOf(model);
         // iterate trough all parents
         while (parentCtor && parentCtor.name !== 'Typegoose' && parentCtor.name !== 'Object') {
             // extend schema
-            schema = model.prototype.buildSchema(parentCtor.name, modelEntry.config.schemaOptions, schema);
+            schema = model.prototype.buildSchema(parentCtor.name, schemaOptions, schema);
             // next parent
             parentCtor = Object.getPrototypeOf(parentCtor);
         }
