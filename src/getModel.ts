c7d5c22b27fc7ab63d6da5ac56b105725c75b52e
Olivier Bourdoux <olivier.bourdoux@gmail.com>
2019-01-30 14:17:20 +0100

    Typo

--- src/getModel.ts
+++ src/getModel.ts
@@ -17,7 +17,7 @@ export function getModel<T extends Typegoose>(connection: Connection, model: Con
         let schema = model.prototype.buildSchema(model, model.name, schemaOptions);
         // get parents class name
         let parentCtor = Object.getPrototypeOf(model);
-        // iterate trough all parents
+        // iterate through all parents
         while (parentCtor && parentCtor.name !== 'Typegoose' && parentCtor.name !== 'Object') {
             // extend schema
             schema = model.prototype.buildSchema(parentCtor, parentCtor.name, schemaOptions, schema);
